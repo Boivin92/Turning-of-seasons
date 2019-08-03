3b36c596ccd381dee35f816c16d8c93cca444c40
Boivin92 <boivin.92@gmail.com>
2019-08-02 22:54:41 -0400

    removed "Game"

--- Level/Level.gd
+++ Level/Level.gd
@@ -2,10 +2,30 @@ extends Node2D
 
 signal objective_got
 
+export (int) var RotationOnAdvance : int = 90
+export (PackedScene) var NextLevel
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_prepare_tween()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$Tween.start()
+		
+func GoToNextLevel():
+	get_tree().change_scene_to(NextLevel)
+
+func _on_Tween_tween_completed(object, key):
+	_prepare_tween()
+
+func _prepare_tween():
+	$Tween.remove_all()
+	$Tween.interpolate_property(self, "rotation_degrees", 
+		rotation_degrees, 
+		rotation_degrees + RotationOnAdvance, 5, 
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($Player, "rotation_degrees", 
+		$Player.rotation_degrees, 
+		$Player.rotation_degrees - RotationOnAdvance, 5, 
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
--- Player/Player.gd
+++ Player/Player.gd
@@ -16,10 +16,17 @@ func _physics_process(delta):
 	if not pounding:
 		get_input()
 	velocity.y = clamp(velocity.y, -2000, 2000)
+	var airborne = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	if airborne && is_on_floor():
+		_shake_camera()
+		
 	if pounding && is_on_floor():
 		pounding = false
-		$Camera2DWithShake.shake(0.4, 15, 8)
+		
+func _shake_camera():
+	$Camera2DWithShake.shake(0.4, 15, 8)
 	
 func get_input():
 	var right = Input.is_action_pressed("right")
