f7585e68e807e04b355df27fb993039409f079b8
Michaël Blanchet <michael.blanchet.2@ulaval.ca>
2019-08-02 21:13:44 -0400

    Ajout de la logique de rotation du level

---
+++ Game/Game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _ready():
+    _prepare_tween()
+
+func _process(delta):
+    if Input.is_action_just_pressed("ui_accept"):
+        $Tween.start()
+
+func _on_Tween_tween_completed(object, key):
+    _prepare_tween()
+
+func _prepare_tween():
+    $Tween.remove_all()
+    $Tween.interpolate_property($Level, "rotation_degrees", 
+        $Level.rotation_degrees, 
+        $Level.rotation_degrees + 90, 5, 
+        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
