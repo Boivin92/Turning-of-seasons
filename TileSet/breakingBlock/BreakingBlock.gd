20b62afed6340ba7c259d2afcff5d3a37245e9c3
Boivin92 <boivin.92@gmail.com>
2019-08-04 13:16:02 -0400

    shrooms and blocks

--- TileSet/breakingBlock/BreakingBlock.gd
+++ TileSet/breakingBlock/BreakingBlock.gd
@@ -1,14 +1,28 @@
 extends StaticBody2D
 
+
 func _on_Timer_timeout() -> void:
 	$CollisionShape2D.disabled = false
 	$Sprite.frame = 0
 	$Sprite.playing = false
 
 func crumble():
-	$Sprite.playing = true
-	CrumblingAudio.PlayWithoutOverride()
+	if $Sprite.animation == "winter":
+		$Sprite.playing = true
+		CrumblingAudio.PlayWithoutOverride()
 
 func _on_Sprite_animation_finished() -> void:
 	$Timer.start()
 	$CollisionShape2D.disabled = true
+	
+func Spring():
+	$Sprite.animation = "spring"
+	
+func Summer():
+	$Sprite.animation = "summer"
+	
+func Fall():
+	$Sprite.animation = "fall"
+	
+func Winter():
+	$Sprite.animation = "winter"
